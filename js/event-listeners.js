b3887ab12faa73de60d48b2fb17ad978acb783a9
noelnp <noel.peltomaki@gmail.com>
2025-04-07 22:50:17 +0200

    Fix navbar bug and console errors

--- js/event-listeners.js
+++ js/event-listeners.js
@@ -4,33 +4,52 @@ document.addEventListener('DOMContentLoaded', function () {
   // Hero section scroll effect
   const heroContainer = document.querySelector('.hero-container');
   const navbar = document.querySelector('.navbar');
-  //const heroHeight = heroContainer.offsetHeight;
 
-  window.addEventListener('scroll', function () {
-    if (window.scrollY > heroHeight - 75) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-  });
+  if (heroContainer && navbar) {
+    const heroHeight = heroContainer.offsetHeight;
+
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > heroHeight - 75) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    });
+  }
+
+  const checkoutLink = document.getElementById('checkoutLink');
+  if (checkoutLink) {
+    checkoutLink.addEventListener('click', function (event) {
+      if (!localStorage.getItem('selectedProduct')) {
+        event.preventDefault();
+        window.location.href = 'index.html';
+
+        alert('Var vänlig och välj en produkt.');
+      }
+    });
+  }
 
   // Checkout-formulär
   const submitButton = document.getElementById('submitButton');
-  submitButton.addEventListener('click', function (event) {
-    event.preventDefault();
+  if (submitButton) {
+    submitButton.addEventListener('click', function (event) {
+      event.preventDefault();
 
-    const firstName = document.getElementById('firstNameInput').value;
-    localStorage.setItem('customerFirstName', firstName);
+      const firstName = document.getElementById('firstNameInput').value;
+      localStorage.setItem('customerFirstName', firstName);
 
-    if (validateCheckoutForm()) {
-      window.location.href = 'checkout-complete.html';
-    }
-  });
+      if (validateCheckoutForm()) {
+        window.location.href = 'checkout-complete.html';
+      }
+    });
+  }
 
   // Ta bort produkt
   const removeProductElement = document.getElementById('removeProduct');
-  removeProductElement.addEventListener('click', function () {
-    localStorage.removeItem('selectedProduct');
-    window.location.href = 'index.html';
-  });
+  if (removeProductElement) {
+    removeProductElement.addEventListener('click', function () {
+      localStorage.removeItem('selectedProduct');
+      window.location.href = 'index.html';
+    });
+  }
 });
